b2eec6078762ef347d1cea0152b13d502b92cb94
danigm11 <danigm821@gmail.com>
2024-02-01 19:28:10 +0100

    Inicio de peticiones http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  API_URL : string = 'https://localhost:7093/';
+
+  myForm: FormGroup;
+  email: string = '';
+  password: string='';
+
+  users: User[] = [];
+
+  constructor(private httpClient: HttpClient,private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+ async uploadCredentials() {
+    const formData = new FormData();
+    formData.append('email', this.myForm.get('email')?.value);
+    formData.append('password', this.myForm.get('password')?.value);
+
+    const request$ = this.httpClient.post<boolean>(`${this.API_URL}api/userlist/`, formData);
+    //const event: any = await lastValueFrom(request$);
+  const edxist = await lastValueFrom(request$);
+
+    alert('Sesión iniciada con éxito');
+  }
+  async updateImageList() {
+    const request$ = this.httpClient.get<User[]>(`${this.API_URL}api/userlist/`);
+    this.users = await lastValueFrom(request$);
+  }
+}
+interface User {
+  email: string;
+  password: string;
 }
