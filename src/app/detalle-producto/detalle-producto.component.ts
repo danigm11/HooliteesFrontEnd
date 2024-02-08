526b65e4c2c314a8fe9c6250f7ecde7a2dd273bd
danigm11 <danigm821@gmail.com>
2024-02-08 03:37:35 +0100

    Refactorización de peticiones get de productos al servicio, funcionalidad del catálogo completa e inicio de la vista detalle producto

--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/Product';
+import { HttpClient } from '@angular/common/http';
+import { Subscription, lastValueFrom } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ServicioService } from '../servicio.service';
 @Component({
   selector: 'app-detalle-producto',
   templateUrl: './detalle-producto.component.html',
   styleUrls: ['./detalle-producto.component.css']
 })
-export class DetalleProductoComponent {
+export class DetalleProductoComponent implements OnInit{
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient,
+    private servicioService: ServicioService
+    ) {
+
+  }
+  unsubs: Subscription | null = null;
+
+  ngOnInit(): void {
+    this.unsubs = this.activatedRoute.params.subscribe((data) => {
+      this.id = data['id'];
+      this.getProducto();
+    })
+  }
+
+  API_URL : string = 'https://localhost:7093/';
+  productoDetalle: any;
+  id: number=0;
+
+  getProducto(){
+    this.servicioService.getProducts().then(products => {
+      products=products.filter((Product) =>
+      Product.id==this.id
+      );
+      this.productoDetalle=products[0];
+    });
+  }
 }
--- src/app/servicio.service.ts
+++ src/app/servicio.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Product } from './model/Product';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  API_URL : string = 'https://localhost:7093/';
+  
+  async getProducts(): Promise<Product[]> {
+    try {
+      const request$ = this.httpClient.get<Product[]>(`${this.API_URL}api/Product/productdetail/`);
+      const products: Product[] = await lastValueFrom(request$);
+      
+      return products;
+    } catch(error) {
+      alert('Seguramente te hayas olvidado de ejecutar la base de datos');
+      console.log(error);
+      return [];
+    }
+  }
 }
